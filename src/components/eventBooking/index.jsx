8dad4de11cfe47fd990a9e474b211246610316f4
Aleksandr <ichmarchuk@gail.com>
2026-02-20 12:23:42 +0100

    changed EventBooking

--- src/components/eventBooking/index.jsx
+++ src/components/eventBooking/index.jsx
@@ -3,6 +3,7 @@ import eventsData from "../eventsData";
 import DateSelector from "../dateSelector";
 import EventSelector from "../eventSelector";
 import SeatSelector from "../seatSelector";
+import styles from "./styles.module.css";
 
 function EventBooking() {
   const [selectedDate, setSelectedDate] = useState(null);
@@ -11,12 +12,34 @@ function EventBooking() {
 
   return (
     <div className={styles.container}>
-      {/* <EventDetails />
+      <DateSelector
+        dates={eventsData}
+        selectedDate={selectedDate}
+        onSelect={(date) => {
+          setSelectedDate(date);
+          setSelectedEvent(null);
+          setSelectedSeats([]);
+        }}
+      />
+
+      <EventSelector
+        events={selectedDate?.events}
+        selectedEvent={selectedEvent}
+        onSelect={(event) => {
+          setSelectedEvent(event);
+          setSelectedSeats([]);
+        }}
+      />
+
       <SeatSelector
+        seats={selectedEvent?.seats}
         selectedSeats={selectedSeats}
         setSelectedSeats={setSelectedSeats}
       />
-      <h4>Selected seats: {selectedSeats.join(", ") || "none"}</h4> */}
+
+      {selectedSeats.length > 0 && (
+        <h4>Selected seats: {selectedSeats.join(", ")}</h4>
+      )}
     </div>
   );
 }
